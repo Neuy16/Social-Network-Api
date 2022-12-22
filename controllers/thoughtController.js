82d2c96b2d71d8df9fead74da969a9403652536e
Neuy <neuwirth.andrew9@gmail.com>
2022-12-21 20:35:19 -0500

    pushing for testing

---
+++ controllers/thoughtController.js
@@ -0,0 +1,17 @@
+const { Thought, User } = require('../models');
+
+// get all thoughts
+const thoughtController = {
+  getThoughts(req, res) {
+    Thought.find()
+      .sort({ createdAt: -1 })
+      .then((dbThoughtData) => {
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
+
+}
